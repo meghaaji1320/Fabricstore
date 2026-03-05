90ade772cd9d5300d52fd63a25930e1a857a4ee7
megha <meghaaji02@gmail.com>
2026-03-05 15:19:16 +0530

    added data

---
+++ src/Context/Data.js
@@ -0,0 +1,92 @@
+export const initialUsers=[
+    {
+        id:1,
+        username:"admin",
+        email:"admin@gmail.com",
+        password:4555,
+        isAdmin:true
+    },
+    {
+        id:2,
+        username:"megha",
+        email:"megha@gmail.com",
+        password:1234,
+        isAdmin:false
+    }
+];
+
+export const initialProducts = [
+    {
+        id:101,
+        title:"gold and silver sequins",
+        price:475,
+        image:"https://doeraa.com/cdn/shop/files/gold-and-silver-sequins-with-mauve-thread-wavy-embroidery-work-on-mauve-soft-net-fabric-386690.jpg?v=1715336163&width=400",
+        description:"Gold And Silver Sequins With Mauve Thread Wavy Embroidery Work On Mauve Soft Net Fabric",
+        category:"net fabrics"
+    },
+    {
+    id:102,
+    title:"Gerogette embroidery",
+    price:1575,
+    image:"https://doeraa.com/cdn/shop/files/big-width-56-multi-thread-with-water-sequins-floral-embroidery-on-peach-viscose-georgette-fabric-620861.jpg?v=1718916233&width=1000",
+    description:"Multi Thread With Water Sequins Floral Embroidery On Peach Viscose Georgette Fabric",
+    category:"Embroidery"
+    },
+    {
+    id:103,
+    title:"White Organza Fabric",
+    price:675,
+    image:"https://doeraa.com/cdn/shop/files/MIT_1394.webp?v=1760854464&width=1000",
+    description:"Gold Zari With Beads Leafy Embroidery Work On brown Organza Fabric",
+    category:"Embroidery"
+    },
+    {
+    id:104,
+    title:"Pink and Beige Fabric",
+    price:780,
+    image:"https://doeraa.com/cdn/shop/products/prints-red-and-yellow-floral-pattern-digital-print-on-grey-mulberry-silk-fabric-39170990440702.jpg?v=1675420914&width=1000",
+    description:"Pink And Beige Beautiful Floral Pattern Digital Print On Grey Mulberry Silk Fabric",
+    category:"Hand Printed"
+    },
+    {
+    id:105,
+    title:"Floral Printed Fabric",
+    price:1275,
+    image:"https://doeraa.com/cdn/shop/files/MIT_0835.webp?v=1760851325&width=1000",
+    description:"Floral Positional Print On Sequins With Zari Embroidery On Tan Beige Organza Fabric",
+    category:"Embroidery"
+    },
+    {
+    id:106,
+    title:"Floral Fabric",
+    price:235,
+    image:"https://doeraa.com/cdn/shop/files/389A8520.webp?v=1742558965&width=1000",
+    description:"Floral Digital Print On Hakoba Cotton Embroidery Fabric",
+    category:"Hakoba"
+    },
+    {
+    id:107,
+    title:"Floral Pink Fabric",
+    price:365,
+    image:"https://doeraa.com/cdn/shop/products/embroidery-fabrics-copy-of-beautiful-floral-hand-painted-with-embroidery-work-on-brown-organza-fabric-38198533488894.jpg?v=1663407899&width=1000",
+    description:"Beautiful Floral Hand Painted With Embroidery Work On Coral Pink Organza Fabric",
+    category:"Hand Printed"
+    },
+    {
+    id:108,
+    title:"Floral Fabric",
+    price:350,
+    image:"https://doeraa.com/cdn/shop/products/embroidery-fabrics-gorgeous-floral-hand-painted-with-foil-work-on-light-peach-organza-fabric-39096993120510.jpg?v=1674214006&width=1000",
+    description:"Gorgeous Floral Hand Painted With Foil Work On Light Peach Organza Fabric",
+    category:"Hand Printed"
+    },
+    {
+    id:109,
+    title:"Maroon Floral Printed",
+    price:375,
+    image:"https://doeraa.com/cdn/shop/products/prints-maroon-beautiful-floral-digital-print-on-cream-organza-fabric-37068598903038.jpg?v=1661391361&width=1000",
+    description:"Maroon Beautiful Floral Digital Print On Cream Organza Fabric",
+    category:"Hand Printed"
+    }
+ 
+]
--- src/Context/ProductContext.jsx
+++ src/Context/ProductContext.jsx
@@ -1,11 +1,50 @@
+// import { createContext, useEffect, useState } from "react";
+
+// export const ProductContext = createContext();
+
+// export const ProductProvider = ({ children }) => {
+//   const [products, setProducts] = useState(
+//     JSON.parse(localStorage.getItem("products")) || []
+//   );
+
+//   useEffect(() => {
+//     localStorage.setItem("products", JSON.stringify(products));
+//   }, [products]);
+
+//   const addProduct = (data) => {
+//     setProducts((prev) => [...prev, { id: Date.now(), ...data }]);
+//   };
+
+//   const deleteProduct = (id) => {
+//     setProducts((prev) => prev.filter((p) => p.id !== id));
+//   };
+
+//   const updateProduct = (id, updatedData) => {
+//     setProducts((prev) =>
+//       prev.map((p) => (p.id === id ? { ...p, ...updatedData } : p))
+//     );
+//   };
+
+//   return (
+//     <ProductContext.Provider
+//       value={{ products, addProduct, deleteProduct, updateProduct }}
+//     >
+//       {children}
+//     </ProductContext.Provider>
+//   );
+// };
+
 import { createContext, useEffect, useState } from "react";
+import { initialProducts } from "./Data";
 
 export const ProductContext = createContext();
 
 export const ProductProvider = ({ children }) => {
-  const [products, setProducts] = useState(
-    JSON.parse(localStorage.getItem("products")) || []
-  );
+
+  const [products, setProducts] = useState(() => {
+    const stored = localStorage.getItem("products");
+    return stored ? JSON.parse(stored) : initialProducts;
+  });
 
   useEffect(() => {
     localStorage.setItem("products", JSON.stringify(products));
